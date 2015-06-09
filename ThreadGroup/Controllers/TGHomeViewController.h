052a2b577773bc44f915d97d09572893748ea7d0
Patrick Butkiewicz <patrick@intrepid.io>
2015-06-09 23:34:30 +0800

    Configure App Delegate to launch home screen controller. Removed storyboards.

---
+++ ThreadGroup/Controllers/TGHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  TGHomeViewController.h
+//  ThreadGroup
+//
+//  Created by Patrick Butkiewicz on 6/9/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGHomeViewController : UIViewController
+
+@end
---
+++ ThreadGroup/Controllers/TGHomeViewController.m
@@ -0,0 +1,21 @@
+//
+//  TGHomeViewController.m
+//  ThreadGroup
+//
+//  Created by Patrick Butkiewicz on 6/9/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGHomeViewController.h"
+
+@interface TGHomeViewController ()
+
+@end
+
+@implementation TGHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
