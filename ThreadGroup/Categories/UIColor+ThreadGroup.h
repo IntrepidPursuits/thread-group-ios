eefc05ca8daf3245e2e2fd524519037305fb7ede
Patrick Butkiewicz <patrick@intrepid.io>
2015-06-09 23:18:50 +0800

    Added UIColor category

---
+++ ThreadGroup/Categories/UIColor+ThreadGroup.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+ThreadGroup.h
+//  ThreadGroup
+//
+//  Created by Patrick Butkiewicz on 6/9/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ThreadGroup)
+
++ (UIColor *)threadGroup_grey;
++ (UIColor *)threadGroup_orange;
+
+@end
---
+++ ThreadGroup/Categories/UIColor+ThreadGroup.m
@@ -0,0 +1,21 @@
+//
+//  UIColor+ThreadGroup.m
+//  ThreadGroup
+//
+//  Created by Patrick Butkiewicz on 6/9/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "UIColor+ThreadGroup.h"
+
+@implementation UIColor (ThreadGroup)
+
++ (UIColor *)threadGroup_grey {
+    return [UIColor colorWithRed:(71.0f/255.0f) green:(72.0f/255.0f) blue:(65.0f/255.0f) alpha:1.0f];
+}
+
++ (UIColor *)threadGroup_orange {
+    return [UIColor colorWithRed:(231.0f/255.0f) green:(124.0f/255.0f) blue:(50.0f/255.0f) alpha:1.0f];
+}
+
+@end
