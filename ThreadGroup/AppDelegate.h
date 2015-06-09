0b351c8050b857a5c6c4a000ab302e5653ac9ba6
Patrick Butkiewicz <patrick@intrepid.io>
2015-06-09 21:39:43 +0800

    Base project commit

---
+++ ThreadGroup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreadGroup
+//
+//  Created by Patrick Butkiewicz on 6/3/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
