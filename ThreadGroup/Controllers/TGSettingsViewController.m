a2ad0e45e8de981a0d9883f673c58c78d6924f81
Patrick Butkiewicz <patrick@intrepid.io>
2015-06-10 01:06:06 +0800

    Added base Settings view controller

---
+++ ThreadGroup/Controllers/TGSettingsViewController.m
@@ -0,0 +1,21 @@
+//
+//  TGSettingsViewController.m
+//  ThreadGroup
+//
+//  Created by Patrick Butkiewicz on 6/10/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGSettingsViewController.h"
+
+@interface TGSettingsViewController ()
+
+@end
+
+@implementation TGSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
