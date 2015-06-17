0880eae738f4a29b406e8288943aca38651cdbdf
LuQuan Intrepid <LuQuan@LuQuans-MacBook-Pro.local>
2015-06-17 15:11:07 -0400

    Added TGTableViewCell
    added labels to xib and expose properties on TGTableViewCell

---
+++ ThreadGroup/Views/TableViewCell/TGTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TGTableViewCell.m
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 6/17/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGTableViewCell.h"
+
+@implementation TGTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
