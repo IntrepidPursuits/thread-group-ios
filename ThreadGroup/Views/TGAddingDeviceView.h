8b180d7b6a1eb8fc441d22b3fd2f22b05702d514
Your Name <you@example.com>
2015-06-22 11:24:13 -0400

    Added TGAddingDeviceView

---
+++ ThreadGroup/Views/TGAddingDeviceView.h
@@ -0,0 +1,13 @@
+//
+//  TGAddingDeviceView.h
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 6/22/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGAddingDeviceView : UIView
+
+@end
