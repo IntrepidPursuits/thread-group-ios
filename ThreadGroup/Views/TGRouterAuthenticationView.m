734187a01f860315296052e70813b990143ad051
LuQuan Intrepid <lu@intrepid.io>
2015-06-23 16:32:48 -0400

    Added TGRouterAuthenticationView

---
+++ ThreadGroup/Views/TGRouterAuthenticationView.m
@@ -0,0 +1,21 @@
+//
+//  TGRouterAuthenticationView.m
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 6/23/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGRouterAuthenticationView.h"
+
+@implementation TGRouterAuthenticationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
