f550d7ea1dc4eb48363c57d26c79d7c6857cc6a4
LuQuan Intrepid <lu@intrepid.io>
2015-06-26 10:31:01 -0400

    Added TGAnimator

---
+++ ThreadGroup/Controllers/AnimatedTransitions/TGAnimator.m
@@ -0,0 +1,13 @@
+//
+//  TGAnimator.m
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 6/26/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGAnimator.h"
+
+@implementation TGAnimator
+
+@end
