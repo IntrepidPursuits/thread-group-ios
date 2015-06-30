55e081c6b3c1410249a187962cd9ca5946632087
LuQuan Intrepid <lu@intrepid.io>
2015-06-30 15:24:46 -0400

    Add popupContentViewController

---
+++ ThreadGroup/Controllers/TGPopupContentViewController.h
@@ -0,0 +1,13 @@
+//
+//  TGPopupContentViewController.h
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 6/30/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGPopupContentViewController : UIViewController
+
+@end
---
+++ ThreadGroup/Controllers/TGPopupContentViewController.m
@@ -0,0 +1,22 @@
+//
+//  TGPopupContentViewController.m
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 6/30/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGPopupContentViewController.h"
+
+@interface TGPopupContentViewController ()
+
+@end
+
+@implementation TGPopupContentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
