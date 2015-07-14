67397b576cc7d6b1c5063c1a4d9cf1290b50bad9
LuQuan Intrepid <lu@intrepid.io>
2015-07-14 18:27:49 -0400

    Added TGNetworkChannelViewController

---
+++ ThreadGroup/Controllers/ThreadNetworkConfig/General/TGNetworkChannelViewController.m
@@ -0,0 +1,22 @@
+//
+//  TGNetworkChannelViewController.m
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 7/14/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGNetworkChannelViewController.h"
+
+@interface TGNetworkChannelViewController ()
+
+@end
+
+@implementation TGNetworkChannelViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
