4e19100cd0f96d98d01102d4844624d4578bc75b
LuQuan Intrepid <lu@intrepid.io>
2015-07-15 14:39:05 -0400

    Added TGNetworkSecurityViewController

---
+++ ThreadGroup/Controllers/ThreadNetworkConfig/General/TGNetworkSecurityViewController.m
@@ -0,0 +1,37 @@
+//
+//  TGNetworkSecurityViewController.m
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 7/15/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGNetworkSecurityViewController.h"
+
+@interface TGNetworkSecurityViewController ()
+
+@end
+
+@implementation TGNetworkSecurityViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
