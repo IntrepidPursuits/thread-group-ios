24a45de7a146d029e88aa7f16580ace7bdcd0cd7
LuQuan Intrepid <lu@intrepid.io>
2015-07-16 11:46:15 -0400

    Added slider cell and time icon

---
+++ ThreadGroup/Views/TableViewCell/ThreadNetworkConfig/Channel/TGNetworkSliderCell.m
@@ -0,0 +1,23 @@
+//
+//  TGNetworkSliderCell.m
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 7/15/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGNetworkSliderCell.h"
+
+@implementation TGNetworkSliderCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
