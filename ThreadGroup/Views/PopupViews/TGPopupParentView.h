a4b40de3c047d3b2488fa00ce547b0b151efb9a3
LuQuan Intrepid <lu@intrepid.io>
2015-07-22 16:30:42 -0400

    Added TGPopupParentView

---
+++ ThreadGroup/Views/PopupViews/TGPopupParentView.h
@@ -0,0 +1,13 @@
+//
+//  TGPopupView.h
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 7/22/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGPopupParentView : UIView
+
+@end
---
+++ ThreadGroup/Views/PopupViews/TGPopupParentView.m
@@ -0,0 +1,14 @@
+//
+//  TGPopupView.m
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 7/22/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGPopupParentView.h"
+
+@implementation TGPopupParentView
+
+
+@end
