50676aaa8e420761b3e26c6232d81b18eba11228
LuQuan Intrepid <lu@intrepid.io>
2015-07-23 16:36:38 -0400

    Added TGPopup. All child popups will subclass this

---
+++ ThreadGroup/Views/PopupViews/TGPopup.h
@@ -0,0 +1,13 @@
+//
+//  TGPopup.h
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 7/23/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGPopup : UIView
+- (instancetype)init;
+@end
