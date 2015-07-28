fba619139c90a727168e49523443463ed0242b76
LuQuan Intrepid <lu@intrepid.io>
2015-07-28 13:56:33 -0400

    Added TGQRCodeParser

---
+++ ThreadGroup/Managers/TGQRCodeParser.h
@@ -0,0 +1,13 @@
+//
+//  TGQRCodeParser.h
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 7/28/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TGQRCodeParser : NSObject
+
+@end
---
+++ ThreadGroup/Managers/TGQRCodeParser.m
@@ -0,0 +1,13 @@
+//
+//  TGQRCodeParser.m
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 7/28/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGQRCodeParser.h"
+
+@implementation TGQRCodeParser
+
+@end
