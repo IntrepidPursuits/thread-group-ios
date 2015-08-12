9b70925f6458f8d939763c7cacbca336687b5156
anbitasiregar <anbitasiregar@gmail.com>
2015-08-12 16:46:42 -0400

    created TGNoWifiVC and subclassed TGNoWifiVC and TGMainVC as TGNavigationVC

---
+++ ThreadGroup/Controllers/TGNoWifiViewController.h
@@ -0,0 +1,13 @@
+//
+//  TGNoWifiViewController.h
+//  ThreadGroup
+//
+//  Created by Anbita Siregar on 8/12/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGNavigationViewController.h"
+
+@interface TGNoWifiViewController : TGNavigationViewController
+
+@end
