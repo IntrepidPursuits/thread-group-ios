0098b6518b379a629dc60f98b57d8e3d70940822
anbitasiregar <anbitasiregar@gmail.com>
2015-08-17 09:59:57 -0400

    added custom navigation animator

---
+++ ThreadGroup/Controllers/AnimatedTransitions/TGNavigationAnimator.m
@@ -0,0 +1,39 @@
+//
+//  TGNavigationAnimator.m
+//  ThreadGroup
+//
+//  Created by Anbita Siregar on 8/14/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGNavigationAnimator.h"
+
+static NSTimeInterval const kTGAnimatorTransitionAnimationDuration = 0.5;
+
+@implementation TGNavigationAnimator
+
+#pragma mark - UIViewControllerAnimatedTransitioning Methods
+
+- (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext {
+    return kTGAnimatorTransitionAnimationDuration;
+}
+
+- (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext {    
+    //Capture state from the transitionContext
+    UIViewController *fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    UIViewController *toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+    
+    UIView *containerView = [transitionContext containerView];
+    [containerView addSubview:toViewController.view];
+    toViewController.view.alpha = 0;
+    
+    [UIView animateWithDuration:kTGAnimatorTransitionAnimationDuration animations:^{
+        fromViewController.view.transform = CGAffineTransformMakeScale(0.1, 0.1);
+        toViewController.view.alpha = 1;
+    } completion:^(BOOL finished) {
+        fromViewController.view.transform = CGAffineTransformIdentity;
+        [transitionContext completeTransition:![transitionContext transitionWasCancelled]];
+    }];
+}
+
+@end
