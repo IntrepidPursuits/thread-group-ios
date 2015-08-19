1a73248e9966ce72bcb5337bfaec65adc5829812
anbitasiregar <anbitasiregar@gmail.com>
2015-08-19 13:47:42 -0400

    log when connection is ignored and fixed bug: noWifiVC to addProductVC still opened keyboard

--- ThreadGroup/Controllers/TGMainViewController.m
+++ ThreadGroup/Controllers/TGMainViewController.m
@@ -205,6 +205,7 @@ static CGFloat const kTGScannerViewAnimationDuration = 0.8f;
             TGSelectDeviceStepViewContentMode completedMode = TGSelectDeviceStepViewContentModeComplete;
             self.selectDeviceView.contentMode = completedMode;
             self.selectDeviceViewHeightLayoutConstraint.constant = [TGSelectDeviceStepView heightForContentMode:completedMode];
+            [self.selectDeviceView resignFirstResponder];
         }
             break;
         default:
@@ -341,6 +342,7 @@ static CGFloat const kTGScannerViewAnimationDuration = 0.8f;
     [self animateConnectingToRouterWithItem:item];
     [[TGNetworkManager sharedManager] connectToRouter:item completion:^(TGNetworkCallbackComissionerPetitionResult *result) {
         if (!self.shouldIgnoreRouterConnection) {
+            NSLog(@"%@ connection was ignored", item.name);
             if (result.hasAuthorizationFailed) {
                 if (![self routerViewIsBeingPresented]) {
                     self.routerAuthVC.item = item;
