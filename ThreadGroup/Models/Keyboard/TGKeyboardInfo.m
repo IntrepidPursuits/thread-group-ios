ae3a6f4d5873921a4adf693857ca61f37a231d9d
LuQuan Intrepid <lu@intrepid.io>
2015-08-21 10:47:53 -0400

    Added TGKeyboardInfo

---
+++ ThreadGroup/Models/Keyboard/TGKeyboardInfo.m
@@ -0,0 +1,13 @@
+//
+//  TGKeyboardInfo.m
+//  ThreadGroup
+//
+//  Created by LuQuan Intrepid on 8/21/15.
+//  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
+//
+
+#import "TGKeyboardInfo.h"
+
+@implementation TGKeyboardInfo
+
+@end
