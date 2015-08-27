42b8fafe86ab73352e3aaf155dd30ae45d2964d4
anbitasiregar <anbitasiregar@gmail.com>
2015-08-27 11:29:03 -0400

    show self.failedConnectionPopup if no result comes back

--- ThreadGroup/Controllers/TGMainViewController.m
+++ ThreadGroup/Controllers/TGMainViewController.m
@@ -232,6 +232,7 @@ static CGFloat const kTGScannerViewAnimationDuration = 0.8f;
     switch (viewState) {
         case TGMainViewStateLookingForRouters:
         case TGMainViewStateConnectingToRouter:
+        case TGMainViewStateFailedRouterConnection:
             self.wifiSearchView.topSeperatorView.hidden = YES;
             self.findingNetworksView.hidden = NO;
             self.availableRoutersView.hidden = NO;
@@ -264,6 +265,7 @@ static CGFloat const kTGScannerViewAnimationDuration = 0.8f;
     switch (viewState) {
         case TGMainViewStateLookingForRouters:
         case TGMainViewStateConnectingToRouter:
+        case TGMainViewStateFailedRouterConnection:
         case TGMainViewStateConnectDeviceTutorial:
         case TGMainViewStateConnectDevicePassphrase:
         case TGMainViewStateConnectDeviceScanning: {
@@ -308,6 +310,10 @@ static CGFloat const kTGScannerViewAnimationDuration = 0.8f;
             break;
         case TGMainViewStateConnectingToRouter: {
             [self.popupView bringChildPopupToFront:self.connectingNetworkPopup animated:animated withCompletion:completion];
+        }
+            break;
+        case TGMainViewStateFailedRouterConnection: {
+            [self.popupView bringChildPopupToFront:self.failedConnectionPopup animated:animated withCompletion:completion];
         }
             break;
         case TGMainViewStateConnectDeviceScanning: {
@@ -364,6 +370,11 @@ static CGFloat const kTGScannerViewAnimationDuration = 0.8f;
     [self.tableView setUserInteractionEnabled:NO];
     [self animateConnectingToRouterWithItem:item];
     [[TGNetworkManager sharedManager] connectToRouter:item completion:^(TGNetworkCallbackComissionerPetitionResult *result) {
+        if (!result) {
+            self.viewState = TGMainViewStateFailedRouterConnection;
+            [self setPopupNotificationForState:TGMainViewStateFailedRouterConnection animated:YES];
+            return;
+        }
         if (result.hasAuthorizationFailed) {
             if (![self routerViewIsBeingPresented] && [self mainViewControllerIsBeingPresented]) {
                 self.routerAuthVC.item = item;
