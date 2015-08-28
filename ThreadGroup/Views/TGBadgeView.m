117cf1701f9b91011c842f70fcc31237ceecbdbf
anbitasiregar <anbitasiregar@gmail.com>
2015-08-28 11:34:22 -0400

    set nibView according to viewStates

--- ThreadGroup/Views/TGBadgeView.m
+++ ThreadGroup/Views/TGBadgeView.m
@@ -6,20 +6,21 @@
 //  Copyright (c) 2015 Intrepid Pursuits. All rights reserved.
 //
 
-#import "TGNotificationView.h"
+#import "TGBadgeView.h"
 #import "UIImage+ThreadGroup.h"
+#import "UIColor+ThreadGroup.h"
 
-@interface TGNotificationView()
+@interface TGBadgeView()
 
 @property (nonatomic) NSInteger count;
 
 @property (nonatomic, strong) UIView *nibView;
 @property (weak, nonatomic) IBOutlet UILabel *countLabel;
-@property (weak, nonatomic) IBOutlet UIView *contentView;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
 
 @end
 
-@implementation TGNotificationView
+@implementation TGBadgeView
 
 - (void)awakeFromNib {
     [super awakeFromNib];
@@ -31,21 +32,34 @@
         [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[bar]-0-|" options:0 metrics:nil views:@{@"bar" : self.nibView}]];
         [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar]-0-|" options:0 metrics:nil views:@{@"bar" : self.nibView}]];
         self.nibView.translatesAutoresizingMaskIntoConstraints = NO;
-        
-        //This is just temporary. In the event we want to put any other views inside contentView, we would need to make a setContentViewWithView:(UIView *)view method
-        [self.contentView addSubview:[[UIImageView alloc] initWithImage:[UIImage tg_connectionFailedAlert]]];
     }
-}
-
-- (void)setBackgroundColor:(UIColor *)color {
-    self.nibView.backgroundColor = color;
 }
 
 #pragma mark - Setters
 
+- (void)setViewState:(TGBadgeViewState)viewState {
+    if (viewState == TGBadgeViewStateCount) {
+        self.nibView.backgroundColor = [UIColor threadGroup_orange];
+        self.countLabel.hidden = NO;
+        self.containerView.hidden = YES;
+    } else if (viewState == TGBadgeViewStateFailed) {
+        self.nibView.backgroundColor = [UIColor threadGroup_red];
+        [self setContainerWithView:[[UIImageView alloc] initWithImage:[UIImage tg_connectionFailedAlert]]];
+        self.countLabel.hidden = YES;
+        self.containerView.hidden = NO;
+    }
+}
+
 - (void)setCount:(NSInteger)count {
     _count = count;
     self.countLabel.text = [@(count) stringValue];
+}
+
+- (void)setContainerWithView:(UIView *)view {
+    [self.containerView addSubview:view];
+    view.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-2-[bar]-2-|" options:0 metrics:nil views:@{@"bar" : view}]];
+    [self.containerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-2-[bar]-2-|" options:0 metrics:nil views:@{@"bar" : view}]];
 }
 
 @end
